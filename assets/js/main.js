7beb4e61e045383f598c53caacdde70800d14767
SmOke11345 <103870835+SmOke11345@users.noreply.github.com>
2022-09-04 15:34:27 +0600

    modified:   assets/css/scss/_normalize.scss 	modified:   assets/css/scss/style.scss 	modified:   assets/css/style.css 	modified:   assets/css/style.css.map 	new file:   assets/img/news--img1.png 	new file:   assets/img/news--img2.png 	new file:   assets/img/news--img3.png 	new file:   assets/img/rent--img1.png 	modified:   assets/js/main.js 	modified:   index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,17 @@
 // carousel
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+    $(".owl-carousel-projects").owlCarousel({
         loop: true,
         items: 2,
         nav: true,
         navText: ["<img src='./assets/img/slider-menu__button.png'>","<img src='./assets/img/slider-menu__button1.png'>"],
         dots: false,
+    });
+    $(".owl-carousel-rent").owlCarousel({
+        dots: false,
+        loop: true,
+        items: 1,
+        nav: true,
+        navText: ["<img src='./assets/img/slider-menu__button.png'>","<img src='./assets/img/slider-menu__button1.png'>"],
     });
 });
